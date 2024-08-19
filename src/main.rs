17363c8229b877544e042e54035a8321244b44ae
salaaad2 <arthurdurant263@gmail.com>
2024-08-19 15:22:00 +0200

    even_fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// sum of all multiples of 3 or 5 from 1 to max
 fn mult_three_or_five(max: i32) -> i32
 {
     let mut ret: i32 = 0;
@@ -10,7 +11,27 @@ fn mult_three_or_five(max: i32) -> i32
     ret
 }
 
+// return the sum of all even numbers in the fibonacci sequence, below max (4.000.000)
+fn even_fibonacci(max: i32) -> i32
+{
+    let mut ret: i32 = 0;
+    let mut sum: i32 = 0;
+    let mut prev: i32 = 1;
+    let mut prev_1: i32 = 0;
+    while ret <= max {
+        ret = prev + prev_1;
+        prev_1 = prev;
+        prev = ret;
+        if ret & 1 == 0
+        {
+            println!("{}", ret);
+            sum += ret;
+        }
+    }
+    sum
+}
 
 fn main() {
     println!("{}", mult_three_or_five(1000));
+    println!("{}", even_fibonacci(4000000));
 }
